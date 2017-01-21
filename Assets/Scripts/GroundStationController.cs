67946933fb19ae01db97bacdd63776489d713f44
davidschep <mail@davidschep.com>
2017-01-21 15:04:46 +0100

    Slider & button

--- Assets/Scripts/GroundStationController.cs
+++ Assets/Scripts/GroundStationController.cs
@@ -16,15 +16,18 @@ namespace GlobalGamejam
         [SerializeField]
         private float m_DishMoveSpeed = 10f;
 
+        private GameObject m_Button;
+
         void Start()
         {
             m_Dish = transform.FindChild("Dish");
+            m_Button = GameObject.Find("UI_Canvas").transform.FindChild("Background Image").FindChild("AntennaSwitchBG").FindChild("AntennaSwitch").gameObject;
             m_Collider2D = transform.FindChild("Collider").GetComponent<BoxCollider2D>();
             m_EnemyManager = GameObject.Find("GameManager").GetComponent<EnemyManager>();
             m_SoundManager = m_EnemyManager.GetComponent<SoundManager>();
         }
 
-        void Update()
+                void Update()
         {
                 if (!m_Collider2D.bounds.Contains(m_EnemyManager.m_FictionalEnemy.transform.position))
                     m_EnemyManager.CreateFictionalEnemy();
@@ -44,6 +47,27 @@ namespace GlobalGamejam
                     if (m_Collider2D.bounds.Contains(m_EnemyManager.m_FictionalEnemy.transform.position) && m_EnemyManager.m_FictionalEnemy.transform.position.x > 5)
                         m_EnemyManager.m_FictionalEnemy.GetComponent<FictionalEnemy>().m_BeepTimer -= m_SoundManager.PlaySpaceShipApproachingSound(m_EnemyManager.m_FictionalEnemy.GetComponent<FictionalEnemy>().m_Difficulty, Vector2.Distance(m_EnemyManager.m_FictionalEnemy.transform.position, transform.position), m_EnemyManager.m_FictionalEnemy.GetComponent<FictionalEnemy>().m_BeepTimer);
                     m_Dish.eulerAngles = new Vector3(0, 0, Mathf.LerpAngle(m_Dish.eulerAngles.z, 45, m_DishMoveSpeed * Time.deltaTime));
+                    break;
+            }
+        }
+
+        private int m_ButtonPosition = 2;
+
+        public void ClickButton()
+        {
+            switch (m_ButtonPosition)
+            {
+                case 1:
+                    m_Button.transform.eulerAngles = new Vector3(0, 0, 0);
+                    m_ButtonPosition = 2;
+                    break;
+                case 2:
+                    m_Button.transform.eulerAngles = new Vector3(0, 0, -55);
+                    m_ButtonPosition = 3;
+                    break;
+                case 3:
+                    m_Button.transform.eulerAngles = new Vector3(0, 0, 55);
+                    m_ButtonPosition = 1;
                     break;
             }
         }
