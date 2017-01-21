094abc3abf11421d9893a2c8387482cc005404f9
davidschep <mail@davidschep.com>
2017-01-21 17:35:04 +0100

    Wave Channel

---
+++ Assets/Scripts/WaveChannel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveChannel : MonoBehaviour
+{
+    public float m_DistortedValue = 1f;
+    public float m_DistortionSpeed = 1f; // in seconds
+    private RectTransform m_Particles;
+
+    private float m_Timer = 0;
+
+    void Start()
+    {
+        m_Particles = transform.GetComponentInChildren<RectTransform>();
+    }
+
+    void Update()
+    {
+        m_Timer -= Time.deltaTime;
+        if (m_Timer < 0)
+        {
+            m_Timer = m_DistortionSpeed;
+            foreach (RectTransform t in m_Particles)
+                t.anchoredPosition = new Vector3(t.anchoredPosition.x, -27 + Random.Range(-m_DistortedValue, m_DistortedValue));
+        }
+    }
+}
