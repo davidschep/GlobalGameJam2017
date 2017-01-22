8300d31efddc26b015c83a10f3fbc0dd61b9c4a7
davidschep <mail@davidschep.com>
2017-01-22 10:29:19 +0100

    Animation - tips windows

--- Assets/AnimationPlayer.cs
+++ Assets/AnimationPlayer.cs
@@ -6,14 +6,19 @@ public class AnimationPlayer : MonoBehaviour
 {
     [SerializeField]
     private Sprite[] m_Sprites = new Sprite[7];
+    [SerializeField]
+    private bool m_Loop = false;
 
     private float m_Timer = 0.0f;
 
     void Update()
     {
         m_Timer += Time.deltaTime;
-        if (m_Timer > 0.7)
-            Destroy(gameObject);
+        if (m_Timer > (float)(m_Sprites.Length - 1) / 10)
+            if (m_Loop)
+                m_Timer = 0;
+            else
+                Destroy(gameObject);
         GetComponent<SpriteRenderer>().sprite = m_Sprites[Mathf.RoundToInt((m_Timer * 10))];
     }
 }
