a63cee75f9acb0e00999630496b33ef45f22d513
davidschep <mail@davidschep.com>
2017-01-22 12:08:16 +0100

    Scene  + wave channel

--- Assets/Scripts/WaveChannel.cs
+++ Assets/Scripts/WaveChannel.cs
@@ -6,13 +6,16 @@ public class WaveChannel : MonoBehaviour
 {
     public float m_DistortedValue = 1f;
     public float m_DistortionSpeed = 1f; // in seconds
-    private RectTransform m_Particles;
+    private RectTransform[] m_Particles;
 
     private float m_Timer = 0;
 
+    [SerializeField]
+    private WaveChannelSetting m_WaveChannelSetting = WaveChannelSetting.OddParticlesRandom;
+
     void Start()
     {
-        m_Particles = transform.GetComponentInChildren<RectTransform>();
+        m_Particles = transform.GetComponentsInChildren<RectTransform>();
     }
 
     void Update()
@@ -21,8 +24,40 @@ public class WaveChannel : MonoBehaviour
         if (m_Timer < 0)
         {
             m_Timer = m_DistortionSpeed;
-            foreach (RectTransform t in m_Particles)
-                t.anchoredPosition = new Vector3(t.anchoredPosition.x, -27 + Random.Range(-m_DistortedValue, m_DistortedValue));
+            // foreach (RectTransform t in m_Particles)
+            switch (m_WaveChannelSetting)
+            {
+                case WaveChannelSetting.FullyRandom:
+                    for (int i = 0; i < m_Particles.Length; i++)
+                        if (m_Particles[i].gameObject.name != "WaveChannel")
+                                m_Particles[i].anchoredPosition = new Vector3(m_Particles[i].anchoredPosition.x, -27 + Random.Range(-m_DistortedValue, m_DistortedValue));
+                    break;
+                case WaveChannelSetting.OddParticlesRandom:
+                    for (int i = 0; i < m_Particles.Length; i++)
+                        if (m_Particles[i].gameObject.name != "WaveChannel")
+                            if (IsOdd(i))
+                                m_Particles[i].anchoredPosition = new Vector3(m_Particles[i].anchoredPosition.x, -27 + Random.Range(-m_DistortedValue, m_DistortedValue));
+
+                    for (int i = 0; i < m_Particles.Length; i++)
+                        if (m_Particles[i].gameObject.name != "WaveChannel")
+                            if (!IsOdd(i))
+                                try
+                                {
+                                    m_Particles[i].anchoredPosition = new Vector3(m_Particles[i].anchoredPosition.x, (m_Particles[i - 1].anchoredPosition.y + m_Particles[i + 1].anchoredPosition.y) / 2);
+                                }
+                                catch { }
+                    break;
+            }
         }
     }
+
+    private bool IsOdd(int value)
+    {
+        return value % 2 != 0;
+    }
 }
+
+public enum WaveChannelSetting
+{
+    FullyRandom, OddParticlesRandom
+};
