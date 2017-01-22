7487182868c32adca0bfb0a8686e06ba76c9012f
davidschep <mail@davidschep.com>
2017-01-22 11:40:12 +0100

    Background noise

---
+++ Assets/Scripts/BackgroundNoise.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundNoise : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip m_Noise;
+    private AudioSource m_NoiseMaker;
+
+    void Start()
+    {
+        m_NoiseMaker = GetComponent<AudioSource>();
+        m_NoiseMaker.clip = m_Noise;
+        m_NoiseMaker.Play();
+    }
+
+    void Update()
+    {
+        if (m_NoiseMaker.isPlaying == false)
+            m_NoiseMaker.Play();
+    }
+}
